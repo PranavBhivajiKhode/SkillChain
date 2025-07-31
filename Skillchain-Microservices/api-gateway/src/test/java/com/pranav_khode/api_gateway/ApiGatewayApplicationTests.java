9109f0759ce4eb2dded63398515d98a1fc9c8a02
PranavBhivajiKhode <pranavedu16@gmail.com>
2025-08-01 00:56:19 +0530

    Api Gateway

---
+++ Skillchain-Microservices/api-gateway/src/test/java/com/pranav_khode/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav_khode.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
